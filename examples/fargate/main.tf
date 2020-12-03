127fc69c6351b6343d3d2a1776ab2257cae0a488
Young-ook <youngook.kim@gmail.com>
2020-12-03 22:53:01 +0900

    feat: AWS Fargate example (#20)

---
+++ examples/fargate/main.tf
@@ -0,0 +1,36 @@
+# Fargate node groups example
+
+terraform {
+  required_version = "0.13.5"
+}
+
+provider "aws" {
+  region              = var.aws_region
+  allowed_account_ids = [var.aws_account_id]
+}
+
+# vpc
+module "vpc" {
+  source             = "terraform-aws-modules/vpc/aws"
+  version            = "2.63.0"
+  name               = var.name
+  azs                = var.azs
+  cidr               = "10.0.0.0/16"
+  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  tags               = module.eks.tags.shared
+}
+
+# eks
+module "eks" {
+  source              = "Young-ook/eks/aws"
+  name                = var.name
+  tags                = var.tags
+  subnets             = module.vpc.private_subnets
+  kubernetes_version  = var.kubernetes_version
+  managed_node_groups = var.managed_node_groups
+  node_groups         = var.node_groups
+  fargate_profiles    = var.fargate_profiles
+}
