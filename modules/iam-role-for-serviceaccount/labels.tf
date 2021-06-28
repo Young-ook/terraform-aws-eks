caa630ac38b3da00c8495a2ed0c8b12c4dd4feaf
mercuriete <mercuriete@yahoo.es>
2021-06-28 10:24:16 +0100

    fix(irsa): irsa expected length of name to be in the range (1 - 64) (#69)
    Co-authored-by: Abel Garcia Dorta <abelgarcia@acidtango.com>

--- modules/iam-role-for-serviceaccount/labels.tf
+++ modules/iam-role-for-serviceaccount/labels.tf
@@ -9,7 +9,7 @@ resource "random_string" "irsa-suffix" {
 
 locals {
   suffix = var.enabled ? random_string.irsa-suffix.0.result : ""
-  name   = var.name == null ? join("-", ["irsa", local.suffix]) : var.name
+  name   = var.name == null ? substr(join("-", ["irsa", local.suffix]), 0, 64) : substr(var.name, 0, 64)
   default-tags = merge(
     { "terraform.io" = "managed" },
     { "Name" = local.name },
