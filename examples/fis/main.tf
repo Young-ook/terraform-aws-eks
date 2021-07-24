e8dbb379951c83bf2182d97ab23bae8f28c72628
Young-ook Kim <youngook.kim@gmail.com>
2021-07-24 13:35:16 +0900

    feat(fis): cloudwatch container-insights alarm

--- examples/fis/main.tf
+++ examples/fis/main.tf
@@ -73,15 +73,18 @@ resource "aws_cloudwatch_metric_alarm" "cpu" {
   alarm_description         = "This metric monitors ec2 cpu utilization"
   tags                      = merge(local.default-tags, var.tags)
   comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = 3
-  metric_name               = "CPUUtilization"
-  namespace                 = "AWS/EC2"
-  period                    = 60
+  datapoints_to_alarm       = 1
+  evaluation_periods        = 1
+  metric_name               = "pod_cpu_utilization"
+  namespace                 = "ContainerInsights"
+  period                    = 30
   statistic                 = "Average"
   threshold                 = 60
   insufficient_data_actions = []
 
   dimensions = {
-    AutoScalingGroupName = local.target_asg
+    Namespace   = "sockshop"
+    Service     = "front-end"
+    ClusterName = module.eks.cluster.name
   }
 }
