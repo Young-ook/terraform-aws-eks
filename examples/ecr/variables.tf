72d25f64a6d22b1ac4215d33f65c28fe8c6d621e
Young-ook Kim <youngook.kim@gmail.com>
2021-08-18 17:53:06 +0900

    feat(ecr): replaced vpc module

--- examples/ecr/variables.tf
+++ examples/ecr/variables.tf
@@ -4,19 +4,43 @@
 variable "aws_region" {
   description = "The aws region to deploy"
   type        = string
-  default     = "us-east-1"
+}
+
+variable "cidr" {
+  description = "The vpc CIDR (e.g. 10.0.0.0/16)"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "azs" {
-  description = "A list of availability zones for the vpc to deploy resources"
+  description = "A list of availability zones for the vpc"
   type        = list(string)
   default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
-variable "subnets" {
-  description = "The list of subnets to deploy an eks cluster"
-  type        = list(string)
+variable "vpc_endpoint_config" {
+  description = "A list of vpc endpoint configurations"
+  type        = list(any)
   default     = null
+}
+
+### features
+variable "enable_igw" {
+  description = "Should be true if you want to provision Internet Gateway for internet facing communication"
+  type        = bool
+  default     = true
+}
+
+variable "enable_ngw" {
+  description = "Should be true if you want to provision NAT Gateway(s) across all of private networks"
+  type        = bool
+  default     = false
+}
+
+variable "single_ngw" {
+  description = "Should be true if you want to provision a single shared NAT Gateway across all of private networks"
+  type        = bool
+  default     = false
 }
 
 ### kubernetes cluster
