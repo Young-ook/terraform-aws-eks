d2bb0eb6778cdc9c17cf844a4f289b729842a906
Young-ook Kim <youngook.kim@gmail.com>
2021-08-27 14:10:45 +0900

    feat(fis): disk stress monitoring

---
+++ examples/fis/aws-cw.tf
@@ -0,0 +1,40 @@
+### application/monitoring
+resource "aws_cloudwatch_metric_alarm" "cpu" {
+  alarm_name                = local.cw_cpu_alarm_name
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  tags                      = merge(local.default-tags, var.tags)
+  metric_name               = "pod_cpu_utilization"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  datapoints_to_alarm       = 1
+  evaluation_periods        = 1
+  namespace                 = "ContainerInsights"
+  period                    = 30
+  threshold                 = 60
+  statistic                 = "Average"
+  insufficient_data_actions = []
+
+  dimensions = {
+    Namespace   = "sockshop"
+    Service     = "front-end"
+    ClusterName = module.eks.cluster.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "disk" {
+  alarm_name                = local.cw_disk_alarm_name
+  alarm_description         = "This metric monitors ec2 disk filesystem usage"
+  tags                      = merge(local.default-tags, var.tags)
+  metric_name               = "node_filesystem_utilization"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  datapoints_to_alarm       = 1
+  evaluation_periods        = 1
+  namespace                 = "ContainerInsights"
+  period                    = 30
+  threshold                 = 60
+  extended_statistic        = "p90"
+  insufficient_data_actions = []
+
+  dimensions = {
+    ClusterName = module.eks.cluster.name
+  }
+}
--- examples/fis/labels.tf
+++ examples/fis/labels.tf
@@ -3,6 +3,7 @@ locals {
   alb_sg_name                     = join("-", [var.name, "alb"])
   alb_aware_sg_name               = join("-", [var.name, "alb-aware"])
   cw_cpu_alarm_name               = join("-", [var.name, "cpu-alarm"])
+  cw_disk_alarm_name              = join("-", [var.name, "disk-alarm"])
   cw_api_p90_alarm_name           = join("-", [var.name, "api-p90-alarm"])
   cw_api_avg_alarm_name           = join("-", [var.name, "api-avg-alarm"])
   asg_target_tracking_policy_name = join("-", [var.name, "target-tracking-autoscaling-policy"])
