2b08106890b36b40b40de3ad52d48107641074ea
mg <grose.matthewf@gmail.com>
2021-10-23 13:51:34 -0500

    Additional Deployment Patterns
    add support for fargate, regions other than us-west-2 and ec2 metadata disabled environments

--- modules/lb-controller/main.tf
+++ modules/lb-controller/main.tf
@@ -3,6 +3,37 @@
 locals {
   namespace      = lookup(var.helm, "namespace", "kube-system")
   serviceaccount = lookup(var.helm, "serviceaccount", "aws-load-balancer-controller")
+  // https://docs.aws.amazon.com/eks/latest/userguide/add-ons-images.html
+  region_to_image_account_mapping = {
+    "af-south-1"     = "877085696533"
+    "ap-east-1"      = "800184023465"
+    "ap-northeast-1" = "602401143452"
+    "ap-northeast-2" = "602401143452"
+    "ap-northeast-3" = "602401143452"
+    "ap-south-1"     = "602401143452"
+    "ap-southeast-1" = "602401143452"
+    "ap-southeast-2" = "602401143452"
+    "ca-central-1"   = "602401143452"
+    "cn-north-1"     = "918309763551"
+    "cn-northwest-1" = "961992271922"
+    "eu-central-1"   = "602401143452"
+    "eu-north-1"     = "602401143452"
+    "eu-south-1"     = "590381155156"
+    "eu-west-1"      = "602401143452"
+    "eu-west-2"      = "602401143452"
+    "eu-west-3"      = "602401143452"
+    "me-south-1"     = "558608220178"
+    "sa-east-1"      = "602401143452"
+    "us-east-1"      = "602401143452"
+    "us-east-2"      = "602401143452"
+    "us-gov-east-1"  = "151742754352"
+    "us-gov-west-1"  = "013241004608"
+    "us-west-1"      = "602401143452"
+    "us-west-2"      = "602401143452"
+  }
+  // if china region, append .cn
+  image_domain      = trimprefix(var.region, "cn-") != var.region ? "amazonaws.com.cn" : "amazonaws.com"
+  region_image_repo = "${lookup(local.region_to_image_account_mapping, var.region, "us-west-2")}.dkr.ecr.${var.region}.${local.image_domain}/amazon/aws-load-balancer-controller"
 }
 
 module "irsa" {
@@ -39,6 +70,9 @@ resource "helm_release" "lbc" {
       "clusterName"                                               = var.cluster_name
       "serviceAccount.name"                                       = local.serviceaccount
       "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn" = module.irsa[0].arn[0]
+      "region"                                                    = var.region
+      "vpcId"                                                     = var.vpc_id
+      "image.repository"                                          = local.region_image_repo
     }
     content {
       name  = set.key
