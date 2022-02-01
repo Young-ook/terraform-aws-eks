49d087f98d413e50574c9eb7d478a0db9c9d426a
youngookkim <youngook.kim@gmail.com>
2022-02-01 15:08:40 +0900

    feat(mng): added capacity_type (#128)

--- defaults.tf
+++ defaults.tf
@@ -4,6 +4,7 @@ locals {
   default_eks_config = {
     ami_type      = "AL2_x86_64"
     instance_type = "t3.medium"
+    capacity_type = null # allowed values: ON_DEMAND, SPOT, and default is null
   }
   default_bottlerocket_config = {
     admin_container_enabled      = false
