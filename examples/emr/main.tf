dee11db6ed33b5fac30bf70fdcde01fdb8061718
Young-ook Kim <youngook.kim@gmail.com>
2022-02-09 17:41:21 +0900

    fix(emr): emr on eks example update with new vpc module

--- examples/emr/main.tf
+++ examples/emr/main.tf
@@ -8,11 +8,26 @@ provider "aws" {
   region = var.aws_region
 }
 
-### application/eks
+# vpc
+module "vpc" {
+  source  = "Young-ook/sagemaker/aws//modules/vpc"
+  version = "> 0.0.6"
+  name    = var.name
+  tags    = var.tags
+  vpc_config = var.use_default_vpc ? null : {
+    azs         = var.azs
+    cidr        = "10.10.0.0/16"
+    subnet_type = "private"
+    single_ngw  = true
+  }
+}
+
+# eks
 module "eks" {
   source              = "Young-ook/eks/aws"
   name                = var.name
   tags                = var.tags
+  subnets             = slice(values(module.vpc.subnets[var.use_default_vpc ? "public" : "private"]), 0, 3)
   kubernetes_version  = var.kubernetes_version
   managed_node_groups = var.managed_node_groups
   enable_ssm          = var.enable_ssm
--- examples/emr/variables.tf
+++ examples/emr/variables.tf
@@ -4,49 +4,16 @@
 variable "aws_region" {
   description = "The aws region to deploy"
   type        = string
-  default     = "us-east-1"
 }
 
-variable "cidr" {
-  description = "The vpc CIDR (e.g. 10.0.0.0/16)"
-  type        = string
-  default     = "10.0.0.0/16"
+variable "use_default_vpc" {
+  description = "A feature flag for whether to use default vpc"
+  type        = bool
 }
 
 variable "azs" {
   description = "A list of availability zones for the vpc to deploy resources"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
-
-variable "vpc_endpoint_config" {
-  description = "A list of vpc endpoint configurations"
-  type        = list(any)
-  default     = null
-}
-
-variable "private_subnets" {
-  description = "A list of CIDRs for private subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "enable_igw" {
-  description = "Should be true if you want to provision Internet Gateway for internet facing communication"
-  type        = bool
-  default     = true
-}
-
-variable "enable_ngw" {
-  description = "Should be true if you want to provision NAT Gateway(s) across all of private networks"
-  type        = bool
-  default     = false
-}
-
-variable "single_ngw" {
-  description = "Should be true if you want to provision a single shared NAT Gateway across all of private networks"
-  type        = bool
-  default     = false
 }
 
 ### kubernetes cluster
