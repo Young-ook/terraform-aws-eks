5de4e4131abd4ca8fd3b7c94507de2e38b17325d
youngookkim <youngook.kim@gmail.com>
2022-04-14 11:44:35 +0900

    feat(appmesh): additional helm vars for app-mesh (#152)
    * feat(appmesh): additional helm vars for app-mesh
    
    * fix(appmesh): remove 'enabled' var for conditional creation
    
    * fix(appmesh): aws partitions expression

--- modules/app-mesh/labels.tf
+++ modules/app-mesh/labels.tf
@@ -1,13 +1,4 @@
-data "aws_partition" "current" {
-  count = var.enabled ? 1 : 0
-}
-
-data "aws_region" "current" {
-  count = var.enabled ? 1 : 0
-}
-
 resource "random_string" "appmesh-suffix" {
-  count   = var.enabled ? 1 : 0
   length  = 5
   upper   = false
   lower   = true
@@ -16,7 +7,7 @@ resource "random_string" "appmesh-suffix" {
 }
 
 locals {
-  suffix = var.petname && var.enabled ? random_string.appmesh-suffix.0.result : ""
+  suffix = var.petname ? random_string.appmesh-suffix.result : ""
   name   = join("-", compact([var.cluster_name, "app-mesh", local.suffix]))
   default-tags = merge(
     { "terraform.io" = "managed" },
--- modules/app-mesh/main.tf
+++ modules/app-mesh/main.tf
@@ -1,4 +1,9 @@
-## kubernetes appmesh
+## kubernetes aws-app-mesh-controller
+
+# aws partitions
+module "aws" {
+  source = "Young-ook/spinnaker/aws//modules/aws-partitions"
+}
 
 locals {
   namespace      = lookup(var.helm, "namespace", "appmesh-system")
@@ -7,21 +12,19 @@ locals {
 
 module "irsa" {
   source         = "../iam-role-for-serviceaccount"
-  count          = var.enabled ? 1 : 0
   name           = join("-", ["irsa", local.name])
   namespace      = local.namespace
   serviceaccount = local.serviceaccount
   oidc_url       = var.oidc.url
   oidc_arn       = var.oidc.arn
   policy_arns = [
-    format("arn:%s:iam::aws:policy/AWSCloudMapFullAccess", data.aws_partition.current.0.partition),
-    format("arn:%s:iam::aws:policy/AWSAppMeshFullAccess", data.aws_partition.current.0.partition),
+    format("arn:%s:iam::aws:policy/AWSCloudMapFullAccess", module.aws.partition.partition),
+    format("arn:%s:iam::aws:policy/AWSAppMeshFullAccess", module.aws.partition.partition),
   ]
   tags = var.tags
 }
 
 resource "helm_release" "appmesh" {
-  count            = var.enabled ? 1 : 0
   name             = lookup(var.helm, "name", "appmesh-controller")
   chart            = lookup(var.helm, "chart", "appmesh-controller")
   version          = lookup(var.helm, "version", null)
@@ -31,13 +34,13 @@ resource "helm_release" "appmesh" {
   cleanup_on_fail  = lookup(var.helm, "cleanup_on_fail", true)
 
   dynamic "set" {
-    for_each = {
-      "region"                                                    = data.aws_region.current.0.name
+    for_each = merge({
+      "region"                                                    = module.aws.region.name
       "serviceAccount.name"                                       = local.serviceaccount
-      "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn" = module.irsa[0].arn
+      "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn" = module.irsa.arn
       "tracing.enabled"                                           = true
       "tracing.provider"                                          = "x-ray"
-    }
+    }, lookup(var.helm, "vars", {}))
     content {
       name  = set.key
       value = set.value
