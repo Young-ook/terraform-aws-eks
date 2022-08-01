3f8ad6ef416259c6445f7545710506a0e891f94c
Young-ook Kim <youngook.kim@gmail.com>
2022-08-01 11:42:28 +0900

    fix(promethes): update prometheus module naming

--- modules/prometheus/variables.tf
+++ modules/prometheus/variables.tf
@@ -2,36 +2,13 @@
 variable "helm" {
   description = "The helm release configuration"
   type        = any
-  default = {
-    name       = "prometheus"
-    repository = "https://prometheus-community.github.io/helm-charts"
-    chart      = "prometheus"
-    namespace  = "prometheus"
-    values = {
-      "alertmanager.persistentVolume.storageClass" = "gp2"
-      "server.persistentVolume.storageClass"       = "gp2"
-    }
-    cleanup_on_fail = true
-    vars            = {}
-  }
+  default     = {}
 }
 
 ### security/policy
 variable "oidc" {
   description = "The Open ID Connect properties"
   type        = map(any)
-}
-
-### description
-variable "cluster_name" {
-  description = "The kubernetes cluster name"
-  type        = string
-}
-
-variable "petname" {
-  description = "An indicator whether to append a random identifier to the end of the name to avoid duplication"
-  type        = bool
-  default     = true
 }
 
 ### tags
