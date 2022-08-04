9365e5913c872de0d236d5d54d2f2551883afbb8
Young-ook Kim <youngook.kim@gmail.com>
2022-08-04 15:18:17 +0900

    fix(ms): update metrics-server module naming

--- modules/metrics-server/main.tf
+++ modules/metrics-server/main.tf
@@ -6,17 +6,18 @@ locals {
 }
 
 resource "helm_release" "metrics" {
-  name            = lookup(var.helm, "name", "metrics-server")
-  chart           = lookup(var.helm, "chart", "metrics-server")
-  version         = lookup(var.helm, "version", null)
-  repository      = lookup(var.helm, "repository", "https://charts.helm.sh/stable")
-  namespace       = local.namespace
-  cleanup_on_fail = lookup(var.helm, "cleanup_on_fail", true)
+  name             = lookup(var.helm, "name", local.default_helm_config["name"])
+  chart            = lookup(var.helm, "chart", local.default_helm_config["chart"])
+  version          = lookup(var.helm, "version", local.default_helm_config["version"])
+  repository       = lookup(var.helm, "repository", local.default_helm_config["repository"])
+  namespace        = local.namespace
+  create_namespace = true
+  cleanup_on_fail  = lookup(var.helm, "cleanup_on_fail", local.default_helm_config["cleanup_on_fail"])
 
   dynamic "set" {
     for_each = merge({
       "args[0]" = "--kubelet-preferred-address-types=InternalIP"
-    }, lookup(var.helm, "vars", {}))
+    }, lookup(var.helm, "vars", local.default_helm_config["vars"]))
     content {
       name  = set.key
       value = set.value
