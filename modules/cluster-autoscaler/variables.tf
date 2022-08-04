a6e530c07e9eaf6b13400f07e31981a6c4b7fb3d
Young-ook Kim <youngook.kim@gmail.com>
2022-08-04 14:54:35 +0900

    fix(cas): update naming rule for cluster autoscaler

--- modules/cluster-autoscaler/variables.tf
+++ modules/cluster-autoscaler/variables.tf
@@ -2,32 +2,13 @@
 variable "helm" {
   description = "The helm release configuration"
   type        = any
-  default = {
-    name            = "cluster-autoscaler"
-    chart           = "cluster-autoscaler"
-    namespace       = "kube-system"
-    serviceaccount  = "cluster-autoscaler"
-    cleanup_on_fail = true
-    vars            = {}
-  }
+  default     = {}
 }
 
 ### security/policy
 variable "oidc" {
   description = "The Open ID Connect properties"
   type        = map(any)
-}
-
-### description
-variable "cluster_name" {
-  description = "The kubernetes cluster name"
-  type        = string
-}
-
-variable "petname" {
-  description = "An indicator whether to append a random identifier to the end of the name to avoid duplication"
-  type        = bool
-  default     = true
 }
 
 ### tags
