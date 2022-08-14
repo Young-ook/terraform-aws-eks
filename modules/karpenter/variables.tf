7dd9c63c91fb82c0d7221b1b1b4b846dcd2f27aa
Young-ook Kim <youngook.kim@gmail.com>
2022-08-14 14:43:34 +0900

    fix(karpenter): update karpenter module naming

--- modules/karpenter/variables.tf
+++ modules/karpenter/variables.tf
@@ -1,35 +1,14 @@
-variable "enabled" {
-  description = "A conditional indicator to enable cluster-autoscale"
-  type        = bool
-  default     = true
-}
-
 ### helm
 variable "helm" {
   description = "The helm release configuration"
   type        = any
-  default = {
-    name            = "karpenter"
-    repository      = "https://charts.karpenter.sh"
-    chart           = "karpenter"
-    namespace       = "karpenter"
-    serviceaccount  = "karpenter"
-    cleanup_on_fail = true
-    vars            = {}
-  }
+  default     = {}
 }
 
 ### security/policy
 variable "oidc" {
   description = "The Open ID Connect properties"
   type        = map(any)
-}
-
-### description
-variable "petname" {
-  description = "An indicator whether to append a random identifier to the end of the name to avoid duplication"
-  type        = bool
-  default     = true
 }
 
 ### tags
