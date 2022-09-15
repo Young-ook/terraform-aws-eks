080afc61d1b45ed10962197d600b8405ec193676
Young-ook Kim <youngook.kim@gmail.com>
2022-09-15 11:20:16 +0900

    feat(helm): helm module

---
+++ modules/helm/main.tf
@@ -0,0 +1,19 @@
+## helm
+
+resource "helm_release" "helm" {
+  name             = lookup(var.helm, "name", local.default_helm_config["name"])
+  chart            = lookup(var.helm, "chart", local.default_helm_config["chart"])
+  version          = lookup(var.helm, "version", local.default_helm_config["version"])
+  repository       = lookup(var.helm, "repository", local.default_helm_config["repository"])
+  namespace        = lookup(var.helm, "namespace", local.default_helm_config["namespace"])
+  create_namespace = lookup(var.helm, "create_namespace", local.default_helm_config["create_namespace"])
+  cleanup_on_fail  = lookup(var.helm, "cleanup_on_fail", local.default_helm_config["cleanup_on_fail"])
+
+  dynamic "set" {
+    for_each = lookup(var.helm, "values", local.default_helm_config["values"])
+    content {
+      name  = set.key
+      value = set.value
+    }
+  }
+}
