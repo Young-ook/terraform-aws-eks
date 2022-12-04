9d3f7e3e2689ed562a7c0de2468336022499947c
youngookkim <youngook.kim@gmail.com>
2022-12-03 21:58:49 -0800

    fix(eks): modify to generate dynamic content inside the eks-addons module (#201)
    * fix(eks): modify to generate dynamic content inside the eks-addons module
    
    * fix(eks): define default eks_name for eks-addons module

--- modules/eks-addons/variables.tf
+++ modules/eks-addons/variables.tf
@@ -1,12 +1,15 @@
-### addon
-variable "addon_config" {
-  description = "EKS Add-on configuration"
-  default     = {}
+### application
+variable "addons" {
+  description = "A configuration list of eks addons"
+  validation {
+    condition     = var.addons != null && length(var.addons) > 0
+    error_message = "Make sure to define valid eks addons configuration."
+  }
 }
 
 ### tags
 variable "tags" {
-  description = "The key-value maps for tagging"
+  description = "The key-value map for tagging"
   type        = map(string)
   default     = {}
 }
