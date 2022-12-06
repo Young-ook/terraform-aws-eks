50e75767152c51128422a1442d758a1471c1bf27
Young-ook Kim <youngook.kim@gmail.com>
2022-12-06 13:12:39 +0900

    feat(blueprint): update codebuild to support multi-arch

--- examples/blueprint/awscp.tf
+++ examples/blueprint/awscp.tf
@@ -1,30 +1,63 @@
 ### container image build pipeline
+locals {
+  projects = [
+    {
+      name      = "hellojs-amd64"
+      arch      = "amd64"
+      image     = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+      type      = "LINUX_CONTAINER"
+      buildspec = "examples/blueprint/apps/hellojs/buildspec.yaml"
+      app_path  = "examples/blueprint/apps/hellojs"
+      repo      = "hellojs"
+    },
+    {
+      name         = "hellojs-arm64"
+      arch         = "arm64"
+      image        = "aws/codebuild/amazonlinux2-aarch64-standard:2.0"
+      compute_type = "BUILD_GENERAL1_LARGE"
+      type         = "ARM_CONTAINER"
+      buildspec    = "examples/blueprint/apps/hellojs/buildspec.yaml"
+      app_path     = "examples/blueprint/apps/hellojs"
+      repo         = "hellojs"
+    },
+    {
+      name      = "yelbv2"
+      image     = "aws/codebuild/standard:5.0"
+      buildspec = "examples/blueprint/apps/yelbv2/buildspec.yml"
+      app_path  = "examples/blueprint/apps/yelbv2"
+      repo      = "yelbv2"
+    },
+  ]
+}
+
 module "ci" {
-  source  = "Young-ook/spinnaker/aws//modules/codebuild"
-  version = "2.3.1"
-  name    = "yelbv2"
-  tags    = var.tags
+  for_each = { for proj in local.projects : proj.name => proj }
+  source   = "Young-ook/spinnaker/aws//modules/codebuild"
+  version  = "2.3.1"
+  name     = each.key
+  tags     = var.tags
   project = {
     source = {
       type      = "GITHUB"
       location  = "https://github.com/Young-ook/terraform-aws-eks.git"
-      buildspec = "examples/blueprint/yelbv2/buildspec.yml"
+      buildspec = lookup(each.value, "buildspec")
       version   = "main"
     }
     environment = {
-      image           = "aws/codebuild/standard:4.0"
+      compute_type    = lookup(each.value, "compute_type", "BUILD_GENERAL1_SMALL")
+      type            = lookup(each.value, "type", "LINUX_CONTAINER")
+      image           = lookup(each.value, "image", "aws/codebuild/standard:4.0")
       privileged_mode = true
       environment_variables = {
-        ARTIFACT_BUCKET = module.artifact.bucket.id
-        REPOSITORY_URI  = module.ecr.url
-        APP_NAME        = "examples/blueprint/yelbv2"
+        APP_PATH       = lookup(each.value, "app_path")
+        ARCH           = lookup(each.value, "arch", "amd64")
+        REPOSITORY_URI = module.ecr[lookup(each.value, "repo")].url
       }
     }
   }
   policy_arns = [
-    module.ecr.policy_arns["read"],
-    module.ecr.policy_arns["write"],
-    module.artifact.policy_arns["write"],
+    module.ecr[lookup(each.value, "repo")].policy_arns["read"],
+    module.ecr[lookup(each.value, "repo")].policy_arns["write"],
   ]
   log = {
     cloudwatch_logs = {
@@ -33,17 +66,11 @@ module "ci" {
   }
 }
 
-module "artifact" {
-  source        = "Young-ook/sagemaker/aws//modules/s3"
-  version       = "0.2.0"
-  name          = "yelbv2"
-  tags          = var.tags
-  force_destroy = true
-}
-
 module "ecr" {
+  for_each     = { for proj in local.projects : proj.repo => proj... }
   source       = "Young-ook/eks/aws//modules/ecr"
-  name         = "yelbv2"
+  version      = "1.7.11"
+  name         = each.key
   scan_on_push = false
 }
 
@@ -62,3 +89,13 @@ module "logs" {
   name      = "yelbv2"
   log_group = each.value.log_group
 }
+
+/*
+resource "local_file" "manifest" {
+  content = templatefile("${path.module}/templates/hello-nodejs.tpl", {
+    ecr_uri = module.ecr.url
+  })
+  filename        = "${path.cwd}/hello-nodejs.yaml"
+  file_permission = "0400"
+}
+*/
