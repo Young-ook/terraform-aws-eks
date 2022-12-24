6a61616f2128607b0b4920622916b621859182e0
youngookkim <youngook.kim@gmail.com>
2022-12-24 13:26:30 +0900

    fix(eks): aws partition module (#231)
    * fix(eks): aws partition module
    
    * feat(eks): update the required terraform version

--- versions.tf
+++ versions.tf
@@ -1,5 +1,7 @@
 ## requirements
 
 terraform {
-  required_version = ">= 0.13"
+  # Terraform test is an experimental features introduced in Terraform CLI v0.15.0.
+  # So, you'll need to upgrade to v0.15.0 or later to use terraform test.
+  required_version = ">= 0.15"
 }
