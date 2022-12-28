330a981f5c67d9e17d20026e9c78cbc118ce0146
Young-ook Kim <youngook.kim@gmail.com>
2022-12-28 09:32:16 +0900

    fix(aws-auth): updata aws-auth module test code

--- modules/aws-auth/tests/defaults/main.tf
+++ modules/aws-auth/tests/defaults/main.tf
@@ -6,10 +6,36 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "ap-northeast-2"
+provider "kubernetes" {
+  alias                  = "aws-auth"
+  host                   = module.eks.kubeauth.host
+  token                  = module.eks.kubeauth.token
+  cluster_ca_certificate = module.eks.kubeauth.ca
+}
+
+### vpc
+module "vpc" {
+  source  = "Young-ook/vpc/aws"
+  version = "1.0.3"
+}
+
+### eks
+module "eks" {
+  source             = "Young-ook/eks/aws"
+  version            = "2.0.0"
+  kubernetes_version = "1.24"
+  subnets            = slice(values(module.vpc.subnets["public"]), 0, 3)
+  managed_node_groups = [
+    {
+      name          = "default"
+      desired_size  = 1
+      instance_type = "t3.medium"
+    },
+  ]
 }
 
 module "main" {
-  source = "../../"
+  depends_on = [module.eks]
+  providers  = { kubernetes = kubernetes.aws-auth }
+  source     = "../../"
 }
