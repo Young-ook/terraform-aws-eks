d41544adbf8069ae0ceb55b3ab9eaaeceb8a32a5
Young-ook Kim <youngook.kim@gmail.com>
2022-12-28 14:59:08 +0900

    fix(eks-addons): prioritize default tags

--- modules/eks-addons/main.tf
+++ modules/eks-addons/main.tf
@@ -5,5 +5,5 @@ resource "aws_eks_addon" "addon" {
   addon_name    = each.key
   addon_version = lookup(each.value, "version", local.default_addon_config["version"])
   cluster_name  = lookup(each.value, "eks_name", local.default_addon_config["eks_name"])
-  tags          = merge({ Name = each.key }, local.default-tags, var.tags)
+  tags          = merge({ Name = each.key }, var.tags, local.default-tags)
 }
