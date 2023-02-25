3176c48590f8a2bec12718c1aa0a3e8f32ba1ecd
youngookkim <youngook.kim@gmail.com>
2023-02-25 10:33:48 +0900

    fix(mlops): grooming eks-kubeflow terraform config (#260)
    * fix(mlops): grooming eks-kubeflow terraform config
    
    * fix(mlops): fix eks role arn for kubeflow install

--- examples/mlops/main.tf
+++ examples/mlops/main.tf
@@ -24,23 +24,29 @@ module "vpc" {
 
 # eks
 module "eks" {
-  source              = "Young-ook/eks/aws"
-  version             = "2.0.3"
-  name                = var.name
-  tags                = var.tags
-  subnets             = slice(values(module.vpc.subnets["private"]), 0, 3)
-  kubernetes_version  = var.kubernetes_version
-  managed_node_groups = var.managed_node_groups
-  node_groups         = var.node_groups
-  fargate_profiles    = var.fargate_profiles
-  enable_ssm          = true
+  source             = "Young-ook/eks/aws"
+  version            = "2.0.3"
+  name               = var.name
+  tags               = var.tags
+  subnets            = slice(values(module.vpc.subnets["private"]), 0, 3)
+  enable_ssm         = true
+  kubernetes_version = var.kubernetes_version
+  managed_node_groups = [
+    {
+      name          = "kubeflow"
+      desired_size  = 5
+      min_size      = 1
+      max_size      = 9
+      instance_type = "t3.medium"
+    }
+  ]
 }
 
 resource "local_file" "kfinst" {
   content = templatefile("${path.module}/templates/kfinst.tpl", {
     aws_region = var.aws_region
     eks_name   = module.eks.cluster.name
-    eks_role   = module.eks.role.arn
+    eks_role   = module.eks.role.managed_node_groups.arn
     kubeconfig = module.eks.kubeconfig
   })
   filename        = "${path.module}/kfinst.sh"
