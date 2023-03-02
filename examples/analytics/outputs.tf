bdc52bd6acb3b5e14681e1039fa58583cac8f130
Young-ook Kim <youngook.kim@gmail.com>
2023-03-02 10:28:20 +0900

    fix(analytics): remove the vpc attribute outputs

--- examples/analytics/outputs.tf
+++ examples/analytics/outputs.tf
@@ -1,8 +1,3 @@
-output "vpc" {
-  description = "The attributes of Amazon VPC"
-  value       = module.vpc.vpc
-}
-
 output "kubeconfig" {
   description = "Bash script to update kubeconfig file"
   value       = module.eks.kubeconfig
