57b9f7ea9e66fb08980c032974d3103918d37223
Young-ook Kim <youngook.kim@gmail.com>
2024-01-07 11:58:33 +0900

    fix(pod-id): trivial update of config description

--- modules/pod-identity/variables.tf
+++ modules/pod-identity/variables.tf
@@ -3,7 +3,7 @@ variable "identities" {
   description = "A configuration list of pod identities"
   validation {
     condition     = var.identities != null && length(var.identities) > 0
-    error_message = "Make sure to define valid pod ids configuration."
+    error_message = "Make sure to define valid pod identity configuration."
   }
 }
 
